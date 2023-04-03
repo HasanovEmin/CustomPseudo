54e1ffd592831835b91f20756817b79cc59d82dd
ehasanov <ehasanov@TM-MRK-NB-0140>
2023-04-03 18:22:03 +0400

    Add project files.

---
+++ PsTrunniCalc/Calculator.cs
@@ -0,0 +1,52 @@
+﻿using Aveva.Core.Database;
+
+namespace PsTrunniCalc
+{
+    public class Calculator
+    {
+        static DbAttribute StextAtt;
+        static DbAttribute SctnStSupAtt;
+        static DbAttribute MemDabDtxrAtt;
+        static DbAttribute PsTrunniCalcAtt;
+        public static void Start()
+        {
+            StextAtt = DbAttribute.GetDbAttribute(":STEXT");
+            SctnStSupAtt = DbAttribute.GetDbAttribute(":SCTNSTSUP");
+            MemDabDtxrAtt = DbAttribute.GetDbAttribute(":MEMDABDTXR");
+            PsTrunniCalcAtt = DbAttribute.GetDbAttribute(":PsTrunniCalc");
+
+            if (StextAtt == null || SctnStSupAtt == null || MemDabDtxrAtt == null || PsTrunniCalcAtt == null)
+                return;
+
+
+            DbPseudoAttribute.GetStringDelegate deleg = new DbPseudoAttribute.GetStringDelegate(Calculating);
+            DbPseudoAttribute.AddGetStringAttribute(PsTrunniCalcAtt, DbElementTypeInstance.TRUNNION, deleg);
+        }
+
+        private static string Calculating(DbElement ele, DbAttribute pseudo, DbQualifier deleg)
+        {
+            var trancies = ele.Members(DbElementTypeInstance.TRANCILLARY);
+            if (trancies == null || trancies.Length == 0)
+            {
+                return "unset";
+            }
+
+            var stext = trancies[0].GetAsString(DbAttributeInstance.STEX);
+            ele.SetAttribute(StextAtt, stext);
+
+            var sctnstsup = trancies[0].GetAsString(SctnStSupAtt);
+            ele.SetAttribute(SctnStSupAtt, sctnstsup);
+
+            var memDab = trancies[0].Members(DbElementTypeInstance.SSREFELEMENT);
+            if (memDab == null || memDab.Length == 0)
+                return "unset";
+
+            var dtxrOfFirstMemdab = memDab[0].GetAsString(DbAttributeInstance.DTXR);
+            ele.SetAttribute(MemDabDtxrAtt, dtxrOfFirstMemdab);
+
+            return "unset";
+
+        }
+
+    }
+}
---
+++ PsTrunniCalc/PseudoStart.cs
@@ -0,0 +1,18 @@
+﻿using Aveva.ApplicationFramework;
+
+namespace PsTrunniCalc
+{
+    public class PseudoStart : IAddin
+    {
+        public string Name => "PsTrunniCalc";
+
+        public string Description => "PsTrunniCalc Calculating Trunni's attributes";
+
+        public void Start(ServiceManager serviceManager)
+        {
+            Calculator.Start();
+        }
+
+        public void Stop() { }
+    }
+}
